b73caf5fdef5271146f41c92b182cdcfd22d8765
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2020-08-07 08:49:56 -0500

    106.[Angular] Parámetros por la URL

--- frontEndAngular/src/app/components/pagina/pagina.component.ts
+++ frontEndAngular/src/app/components/pagina/pagina.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-pagina',
@@ -8,11 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class PaginaComponent implements OnInit {
   public title: string;
 
-  constructor() {
+  public nombre: string;
+  public apellido: string;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router) {
     this.title = "Página de pruebas";
   }
 
   ngOnInit(): void {
+    this._route.params.subscribe((params: Params)=>{
+      console.log(params);
+      this.nombre = params.nombre;
+      this.apellido = params.apellido;
+    });
   }
 
 }
