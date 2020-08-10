41efffd4bae0d6cbd2a9aa664faed4c5ce9f4101
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2020-08-10 08:45:58 -0500

    108.[Angular] Directivas ngIf y ngFor

--- frontEndAngular/src/app/components/peliculas/peliculas.component.ts
+++ frontEndAngular/src/app/components/peliculas/peliculas.component.ts
@@ -9,9 +9,15 @@ export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
 
   public titulo: string;
 
+  public peliculas: Array<any>;
+
   constructor() {
     this.titulo = "Componente Peliculas";
-    console.log("Constructor lanzado");
+    this.peliculas = [
+      {title: "Spiderman 4", image:'https://www.liderempresarial.com/wp-content/uploads/2019/09/Spiderman.jpg'},
+      {title: "Los Vengadores Endgame", image:'https://as.com/tikitakas/imagenes/2019/04/26/portada/1556258369_131914_1556258703_noticia_normal.jpg'},
+      {title: "Batman vs Superman", image:'https://lh3.googleusercontent.com/proxy/ollafJxAW7-CRNkn0iKeyoXXBABNJBDK0cuZ_1cRpaATgXWRswyyM4nloXioh8-XpjxMDcpC_54b899oJgpUx-PuoO4CBSbNj7s5VAg3X_BF0Of928WKomN6V-Hyef060lAO27WEj71HR3VOpyqgvdtON5N2bU-VlGLHXuJaQN7_9qqzP9b-castAM8mWyGy1nTsCDTf5UQF-stTD_PXehWu4uzmjEfE0aWgOWIf'}
+    ];
   }
 
   cambiarTitulo(){
