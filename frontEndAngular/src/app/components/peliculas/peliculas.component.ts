dc1c1c04b08da82be600be7d4a9fc3ce182ed6b3
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2020-08-11 23:53:51 -0500

    111.[Angular] Modelos

--- frontEndAngular/src/app/components/peliculas/peliculas.component.ts
+++ frontEndAngular/src/app/components/peliculas/peliculas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
+import { Pelicula } from 'src/app/models/pelicula';
 
 @Component({
   selector: 'app-peliculas',
@@ -9,14 +10,15 @@ export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
 
   public titulo: string;
 
-  public peliculas: Array<any>;
+  // public peliculas: Array<any>;
+  public peliculas: Array<Pelicula>;
 
   constructor() {
     this.titulo = "Componente Peliculas";
     this.peliculas = [
-      {year: 2019, title: "Spiderman 4", image:'https://www.liderempresarial.com/wp-content/uploads/2019/09/Spiderman.jpg'},
-      {year: 2018, title: "Los Vengadores Endgame", image:'https://as.com/tikitakas/imagenes/2019/04/26/portada/1556258369_131914_1556258703_noticia_normal.jpg'},
-      {year: 2015, title: "Batman vs Superman", image:'https://lh3.googleusercontent.com/proxy/ollafJxAW7-CRNkn0iKeyoXXBABNJBDK0cuZ_1cRpaATgXWRswyyM4nloXioh8-XpjxMDcpC_54b899oJgpUx-PuoO4CBSbNj7s5VAg3X_BF0Of928WKomN6V-Hyef060lAO27WEj71HR3VOpyqgvdtON5N2bU-VlGLHXuJaQN7_9qqzP9b-castAM8mWyGy1nTsCDTf5UQF-stTD_PXehWu4uzmjEfE0aWgOWIf'}
+      new Pelicula("Spiderman 4", 2015, 'https://www.liderempresarial.com/wp-content/uploads/2019/09/Spiderman.jpg'),
+      new Pelicula("Los Vengadores Endgame", 2019,'https://as.com/tikitakas/imagenes/2019/04/26/portada/1556258369_131914_1556258703_noticia_normal.jpg'),
+      new Pelicula("Batman vs Superman", 2015, 'https://lh3.googleusercontent.com/proxy/ollafJxAW7-CRNkn0iKeyoXXBABNJBDK0cuZ_1cRpaATgXWRswyyM4nloXioh8-XpjxMDcpC_54b899oJgpUx-PuoO4CBSbNj7s5VAg3X_BF0Of928WKomN6V-Hyef060lAO27WEj71HR3VOpyqgvdtON5N2bU-VlGLHXuJaQN7_9qqzP9b-castAM8mWyGy1nTsCDTf5UQF-stTD_PXehWu4uzmjEfE0aWgOWIf')
     ];
   }
 
