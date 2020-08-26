253ac9948e00dc1ea7b26468f7709e1c17795e6c
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2020-08-25 23:59:23 -0500

    117.[Angular] Pipes

--- frontEndAngular/src/app/components/peliculas/peliculas.component.ts
+++ frontEndAngular/src/app/components/peliculas/peliculas.component.ts
@@ -13,6 +13,7 @@ export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
   // public peliculas: Array<any>;
   public peliculas: Array<Pelicula>;
   public favorita: Pelicula;
+  public fecha: any;
 
   constructor() {
     this.titulo = "Películas";
@@ -21,6 +22,7 @@ export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
       new Pelicula("Los Vengadores Endgame", 2019,'https://as.com/tikitakas/imagenes/2019/04/26/portada/1556258369_131914_1556258703_noticia_normal.jpg'),
       new Pelicula("Batman vs Superman", 2015, 'https://lh3.googleusercontent.com/proxy/ollafJxAW7-CRNkn0iKeyoXXBABNJBDK0cuZ_1cRpaATgXWRswyyM4nloXioh8-XpjxMDcpC_54b899oJgpUx-PuoO4CBSbNj7s5VAg3X_BF0Of928WKomN6V-Hyef060lAO27WEj71HR3VOpyqgvdtON5N2bU-VlGLHXuJaQN7_9qqzP9b-castAM8mWyGy1nTsCDTf5UQF-stTD_PXehWu4uzmjEfE0aWgOWIf')
     ];
+    this.fecha = new Date(2020, 8, 12);
   }
 
   cambiarTitulo(){
