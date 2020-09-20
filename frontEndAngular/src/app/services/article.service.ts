d6b484b99abd58d657adf48742e0150e8b918f39
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2020-09-19 22:15:17 -0500

    133.[Angular] Buscador - Parte 1

--- frontEndAngular/src/app/services/article.service.ts
+++ frontEndAngular/src/app/services/article.service.ts
@@ -32,4 +32,8 @@ export class ArticleService{
   getArticle(articleId):Observable<any>{
     return this._http.get(this.url+'article/'+articleId);
   }
+
+  search(searchString):Observable<any>{
+    return this._http.get(this.url+'search/'+searchString);
+  }
 }
