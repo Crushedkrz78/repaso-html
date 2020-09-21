6c33868d6704b4ff9c979db881a3b9e13cccdb04
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2020-09-20 19:32:24 -0500

    134.[Angular] Buscador - Parte 2

--- frontEndAngular/src/app/components/search/search.component.ts
+++ frontEndAngular/src/app/components/search/search.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Article } from '../../models/article';
+import { ArticleService } from '../../services/article.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers: [ArticleService]
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  public articles: Article[];
+  public search: string;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _articleService: ArticleService
+  ) { }
 
   ngOnInit(): void {
+    this._route.params.subscribe( params => {
+      var search = params['search'];
+      this.search = search;
+
+      this._articleService.search(search).subscribe(
+        response => {
+          //console.log(response);
+          if(response.articlesFound){
+            this.articles = response.articlesFound;
+            //console.log(this.articles);
+          }
+        }, error => {
+          console.log(error);
+          //this._router.navigate(['/home']);
+          this.articles = [];
+        }
+      );
+    });
   }
 
 }
