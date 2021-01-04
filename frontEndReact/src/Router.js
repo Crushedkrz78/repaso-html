6bdacc0bfac083909d78973466c5cdf63875b6a0
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-01-03 22:55:57 -0600

    171.[React] Ruta 404

--- frontEndReact/src/Router.js
+++ frontEndReact/src/Router.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SeccionPruebas from './components/SeccionPruebas';
 import MiComponente from './components/MiComponente';
 import Peliculas from './components/Peliculas';
+import Error from './components/Error';
 
 class Router extends Component {
 
@@ -16,6 +17,8 @@ class Router extends Component {
                     <Route exact path="/" component={Peliculas}/>
                     <Route exact path="/ruta-prueba" component={SeccionPruebas}/>
                     <Route exact path="/segunda-ruta" component={MiComponente}/>
+
+                    <Route component={Error}/>
                 </Switch>
             </BrowserRouter>
         );
