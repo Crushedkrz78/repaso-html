2e393e5371706f471b16b17ecbf171e00918f49d
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-01-06 21:53:52 -0600

    179.[React] Formularios

--- frontEndReact/src/components/Formulario.js
+++ frontEndReact/src/components/Formulario.js
@@ -4,17 +4,50 @@ import Sidebar from './Sidebar';
 
 class Formulario extends Component{
 
+    nombreRef = React.createRef();
+    apellidosRef = React.createRef();
+    bioRef = React.createRef();
+    generoHombreRef = React.createRef();
+    generoMujerRef = React.createRef();
+    generoOtroRef = React.createRef();
+
+    recibirFormulario = (e) => {
+        e.preventDefault();
+        
+        console.log("Formulario enviado!!");
+        console.log(this.nombreRef.current.value);
+    }
+
     render(){
 
         return(
             <div id="formulario">
-                <Slider 
-                    title="Formulario"
-                    size="slider-small"
-                />
                 <div className="center">
                     <div id="content">
+                        <h1 className="subheader">Formulario</h1>
                         {/*Listado de artículos de la API */}
+                        <form className="mid-form" onSubmit={this.recibirFormulario}>
+                            <div className="form-group">
+                                <label htmlFor="nombre">Nombre</label>
+                                <input type="text" name="nombre" ref={this.nombreRef}/>                            
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="apellidos">Apellidos</label>
+                                <input type="text" name="apellidos" ref={this.apellidosRef}/>                            
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="bio">Biografía</label>
+                                <textarea name="bio" ref={this.bioRef}></textarea>
+                            </div>
+                            <div className="form-group radio-buttons">
+                                <input type="radio" name="genero" value="hombre" ref={this.generoHombreRef}/>Hombre
+                                <input type="radio" name="genero" value="mujer" ref={this.generoMujerRef}/>Mujer
+                                <input type="radio" name="genero" value="otro" ref={this.generoOtroRef}/>Otro 
+                            </div>
+                            <div className="clearfix"></div>
+                            <input type="submit" value="Enviar" className="btn btn-success"/>
+
+                        </form>
                     </div>
 
                     <Sidebar
