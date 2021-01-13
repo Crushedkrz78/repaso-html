afa3e1194979d6c013738cfbf15ca257847480c4
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-01-13 00:49:46 -0600

    183.[React] Introducción a HTTP y peticiones asíncronas

--- frontEndReact/src/components/Blog.js
+++ frontEndReact/src/components/Blog.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Slider from './Slider';
 import Sidebar from './Sidebar';
 
 class Blog extends Component{
 
+    state = {
+        articles: {},
+        status: 'failed'
+    }
+
     render(){
+
+        // Petición AJAX de prueba
+        axios.get("http://localhost:3900/api/articles")
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    articles: res.data.articles,
+                    status: 'success'
+                });
+            });
 
         return(
             <div id="blog">
@@ -16,6 +32,16 @@ class Blog extends Component{
                     <div id="content">
                         <h1 className="subheader">Últimos artículos</h1>
                         {/*Listado de artículos de la API */}
+                        {this.state.status === 'success' &&
+                            <div>
+                                {this.state.articles.map((article) => {
+                                    return(
+                                        <h1 key={article._id}>{article.title}</h1>
+                                    );
+                                })}
+                            </div>
+                        }
+                        
                     </div>
 
                     <Sidebar
