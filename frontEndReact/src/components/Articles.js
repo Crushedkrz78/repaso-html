5022a4bed99c54b1382362b24a3118633dfde7cd
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-01-13 23:37:22 -0600

    187.[React] Mostrar listado de artículos

--- frontEndReact/src/components/Articles.js
+++ frontEndReact/src/components/Articles.js
@@ -28,11 +28,30 @@ class Articles extends Component {
 
     render(){
         if(this.state.articles.length >= 1){
+
+            var listArticles = this.state.articles.map((article) => {
+                return(
+                    <article className="article-item" id="article-template">
+                        <div className="image-wrap">
+                            <img src="https://via.placeholder.com/250x180" alt="placeholder"/>
+                        </div>
+                        <h2>{article.title}</h2>
+                        <span className="date">
+                            {article.date}
+                        </span>
+                        <a href="#">Leer más</a>
+                        
+                        <div className="clearfix"></div>
+                    </article>
+                );
+            });
+            
             return(
                 <div id="articles">
-                    <h1>Listado de artículos</h1>
+                    {listArticles}
                 </div>
             );
+
         }else if(this.state.articles.length === 0 && this.state.status === 'success'){
             return(
                 <div id="articles">
