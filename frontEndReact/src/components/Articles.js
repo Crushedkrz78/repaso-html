fd3c73f55b1de2d1a8cbc40d7b5bee88e502ac4a
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-01-13 23:12:39 -0600

    185.[React] Petición de artículos por HTTP GET

--- frontEndReact/src/components/Articles.js
+++ frontEndReact/src/components/Articles.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Articles extends Component {
+
+    state = {
+        articles: [],
+        status: null
+    };
+
+    componentWillMount(){
+        this.getArticles();
+    }
+
+    getArticles = () => {
+        // console.log("getArticles");
+        axios.get("http://localhost:3900/api/articles")
+            .then(res => {
+                //console.log(res.data);
+                this.setState({
+                    articles: res.data.articles,
+                    status: 'success'
+                });
+
+                console.log(this.state);
+            });
+    }
+
     render(){
         return(
             <div id="articles">
