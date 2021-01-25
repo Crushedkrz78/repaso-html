c2d4c632871024b07bf9120fc684cc31b086f4de
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-01-24 22:51:21 -0600

    199.[React] Formulario de creación

--- frontEndReact/src/components/CreateArticle.js
+++ frontEndReact/src/components/CreateArticle.js
@@ -6,19 +6,46 @@ import Sidebar from './Sidebar';
 
 // Validación de formularios y alertas
 class CreateArticle extends Component{
+
+    titleRef = React.createRef();
+    contentRef = React.createRef();
+
+    state = {
+        article: {},
+        status: null
+    };
+
+    changeState = () => {
+        this.setState({
+            article:{
+                title: this.titleRef.current.value,
+                content: this.contentRef.current.value
+            }
+        });
+
+        console.log(this.state);
+    }
+
+    saveArticle = (e) => {
+        e.preventDefault();
+
+        //Rellenar State con datos del formulario
+        this.changeState();
+    }
+
     render(){
         return(
             <div className="center">
                 <section id="content">
                     <h1 className="subjeader">Crear Artículo</h1>
-                    <form className="mid-form">
+                    <form className="mid-form" onSubmit={this.saveArticle}>
                         <div className="form-group">
                             <label htmlFor="title">Título</label>
-                            <input type="text" name="title"/>
+                            <input type="text" name="title" ref={this.titleRef} onChange={this.changeState}/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="content">Contenido</label>
-                            <textarea name="content"></textarea>
+                            <textarea name="content" ref={this.contentRef} onChange={this.changeState}></textarea>
                         </div>
                         <div className="form-group">
                             <label htmlFor="file0">Imagen</label>
