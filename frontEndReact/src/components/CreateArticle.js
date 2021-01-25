3c360da30dfec8fdff7d20e8da7a6a1b62678e4d
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-01-24 22:26:16 -0600

    198.[React] Formulario para crear Posts

--- frontEndReact/src/components/CreateArticle.js
+++ frontEndReact/src/components/CreateArticle.js
@@ -11,6 +11,22 @@ class CreateArticle extends Component{
             <div className="center">
                 <section id="content">
                     <h1 className="subjeader">Crear Artículo</h1>
+                    <form className="mid-form">
+                        <div className="form-group">
+                            <label htmlFor="title">Título</label>
+                            <input type="text" name="title"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="content">Contenido</label>
+                            <textarea name="content"></textarea>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="file0">Imagen</label>
+                            <input type="file" name="file0"/>
+                        </div>
+
+                        <input type="submit" value="guardar" className="btn btn-succes"/>
+                    </form>
                 </section>
                 <Sidebar/>
             </div>
