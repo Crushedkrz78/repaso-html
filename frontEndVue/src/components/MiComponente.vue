191ae91255f3a9c3ee19593f541e7a23488cb59d
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-01-30 01:18:23 -0600

    211.[Vue] Cómo crear un componente en vue

---
+++ frontEndVue/src/components/MiComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="MiComponente">
+        <hr/>
+        <h1>Este es mi Componente</h1>
+        <h2>{{subtitulo}}</h2>
+        <hr/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MiComponente',
+    data(){
+        return {
+            subtitulo: 'Soy el subtítulo de mi componente'
+        }
+    }
+}
+</script>
