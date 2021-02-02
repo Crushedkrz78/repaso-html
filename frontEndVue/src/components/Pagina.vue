a7ffbc01b019813bb1836f5fcf93444eb216889d
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-02-01 23:38:39 -0600

    220.[Vue] Hooks o elementos del ciclo de vida

--- frontEndVue/src/components/Pagina.vue
+++ frontEndVue/src/components/Pagina.vue
@@ -2,18 +2,37 @@
     <section id="content">
         <h2 class="subheader">Página de pruebas</h2>
         <h3>{{ id_desde_url }}</h3>
+        <button @click="actualizarNombre()">Actualizar Nombre</button>
+        <h4>{{ nombre }}</h4>
     </section>
 </template>
 
 <script>
 export default {
     name: 'Pagina',
+    created(){
+        console.log("Componente Creado");
+    },
     mounted(){
         this.id_desde_url = this.$route.params.id;
+        console.log("Componente Montado");
+    },
+    updated(){
+        console.log("Componente Actualizado")
+    },
+    destroyed(){
+        console.log("Componente Eliminado");
     },
     data(){
         return{
-            id_desde_url: null
+            id_desde_url: null,
+            nombre: 'Cristian Perez'
+        }
+    },
+    methods:{
+        actualizarNombre(){
+            this.nombre = 'CRISTIAN PEREZ';
+            console.log(this.nombre);
         }
     }
 }
