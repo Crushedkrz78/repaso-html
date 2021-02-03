f6881389aa6202205b5cb8cf76be5d9d51d7b64c
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-02-02 23:50:05 -0600

    222.[Vue] Items del menú activo

--- frontEndVue/src/components/HeaderComponent.vue
+++ frontEndVue/src/components/HeaderComponent.vue
@@ -13,19 +13,19 @@
                     <!--Enlaces de navegacion-->
                     <ul>
                         <li>
-                            <router-link to="/home">Inicio</router-link>
+                            <router-link to="/home" active-class="active">Inicio</router-link>
                         </li>
                         <li>
-                            <router-link to="/blog">Blog</router-link>
+                            <router-link to="/blog" active-class="active">Blog</router-link>
                         </li>
                         <li>
-                            <router-link to="/formulario">Formulario</router-link>
+                            <router-link to="/formulario" active-class="active">Formulario</router-link>
                         </li>
                         <li>
-                            <router-link :to="{name: 'pagina', params: {id: 'Soy el valor del parametro'}}">Página</router-link>
+                            <router-link :to="{name: 'pagina', params: {id: 'Soy el valor del parametro'}}" active-class="active">Página</router-link>
                         </li>
                         <li>
-                            <router-link to="/pagina">Página 2</router-link>
+                            <router-link to="/pagina" active-class="active">Página 2</router-link>
                         </li>
                     </ul>
                 </nav>
