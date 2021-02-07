df81d33aadab609bc54fd87179c40170c2a2e3ce
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-02-06 22:57:22 -0600

    229.[Vue] Binding condicional

--- frontEndVue/src/components/Peliculas.vue
+++ frontEndVue/src/components/Peliculas.vue
@@ -8,7 +8,10 @@
                     <img :src="pelicula.image" :alt="pelicula.title" :title="pelicula.title" />
                 </div>
                 <h2>{{pelicula.title}}</h2>
-                <span class="date">
+                <span class="date" :class="{
+                    'yeargreen': pelicula.year >= 2015,
+                    'yearred': pelicula.year < 2015
+                }">
                     {{pelicula.year}}
                 </span>
                 <a href="#">Leer más</a>
