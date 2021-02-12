e2638c387bd79e0778c51869d44e4b681dfc0815
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2021-02-11 23:47:24 -0600

    237.[Vue] Validación de formularios

--- frontEndVue/src/components/Formulario.vue
+++ frontEndVue/src/components/Formulario.vue
@@ -7,14 +7,32 @@
                         <div class="form-group">
                             <label for="nombre">Nombre</label>
                             <input type="text" name="nombre" v-model="user.nombre"/>
+                            <div v-if="!$v.user.nombre.required">
+                                Este campo es requerido
+                            </div>
+                            <div v-if="!$v.user.nombre.minLength">
+                                Este campo debe tener más caracteres
+                            </div>
                         </div>
                         <div class="form-group">
                             <label for="apellidos">Apellidos</label>
                             <input type="text" name="apellidos" v-model="user.apellidos"/>
+                            <div v-if="!$v.user.apellidos.required">
+                                Este campo es requerido
+                            </div>
+                            <div v-if="!$v.user.apellidos.minLength">
+                                Este campo debe tener más caracteres
+                            </div>
                         </div>
                         <div class="form-group">
                             <label for="bio">Biografía</label>
                             <textarea name="bio" v-model="user.biografia"></textarea>
+                            <div v-if="!$v.user.bio.required">
+                                Este campo es requerido
+                            </div>
+                            <div v-if="!$v.user.bio.minLength">
+                                Este campo debe tener más caracteres
+                            </div>
                         </div>
                         <div class="form-group radio-buttons">
                             <input type="radio" name="genero" value="hombre" checked v-model="user.genero"/>Hombre
@@ -36,18 +54,36 @@
 </template>
 
 <script>
+import { required, minLength } from 'vuelidate/lib/validators';
 import Sidebar from './Siderbar.vue';
 export default {
     name: 'Formulario',
     components: {
         Sidebar
     },
+    validations: {
+        user: {
+            nombre: {
+                required,
+                minLength: minLength(2)
+            },
+            apellidos: {
+                required,
+                minLength: minLength(2)
+            },
+            bio: {
+                required,
+                minLength: minLength(10)
+            },
+        }
+    },
     data(){
         return {
+            submitted: false,
             user: {
                 nombre:'',
                 apellidos: '',
-                biografia: '',
+                bio: '',
                 genero: ''
             }
         }
@@ -55,6 +91,12 @@ export default {
     methods: {
         mostrarUsuario(){
             console.log(this.user);
+            this.submitted = true;
+            this.$v.$touch();
+            if(this.$v.$invalid){
+                return false;
+            }
+            alert('Validación pasada');
         }
     }
 }
